1bdc96dc5e07ebd11f694382cf1d2f1d465df77b
camilo escobar <camiloooo.escobar@gmail.com>
2023-07-19 20:17:24 -0400

    simulador de promedio de edades pagina csgo

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-let respuesta = prompt("¿Cuál es tu edad?");
+let edades = [];
 
-if(respuesta){
-    alert("¡Muchas gracias! Queremos conocer a quienes están interesados en CS:GO :)")
+function calcularPromedio(edades) {
+    if (edades.length === 0) {
+        return 0; 
+    }
+    let sumaEdades = edades.reduce((total, edad) => total + edad, 0);
+    return sumaEdades / edades.length;
 }
 
+function registrarEdad() {
+    let nombre = prompt("¿Cuál es tu nombre?");
+    let edad = prompt("¿Cuál es tu edad?");
+    edad = parseInt(edad); 
 
-console.log()
+    if (edad && !isNaN(edad)) { 
+        alert("¡Muchas gracias! Queremos conocer a quienes están interesados en CS:GO :)");
+        edades.push(edad); 
+    } else {
+        alert("Por favor, ingresa una edad válida.");
+    }
+
+    let promedio = calcularPromedio(edades);
+    console.log("Edades registradas:", edades);
+    console.log("Edad promedio:", promedio);
+}
+
+registrarEdad();
